a7b742c5a5113d74c6eb790de2ba53493bfb6287
sudharshanFSD <trssudharshan@gmail.com>
2025-06-19 12:52:31 +0530

    netlify originURL

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://krishtalball.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
